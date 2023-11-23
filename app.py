f54a081398785bc826351e4f20ac8eb7966b2686
vm00328 <vmanolov2000@gmail.com>
2023-11-23 16:37:37 +0100

    Varying Portfolio Sizes + Risk-Return Graph Optimization

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ post_2000_data = data[data['Vintage'] >= 2000].copy()
 emerging_funds = post_2000_data[post_2000_data['Emerging FM'] == 'Yes']
 established_funds = post_2000_data[post_2000_data['Established FM'] == 'Yes']
 
-def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_established_funds, manager_type, returns_column = 'MOIC', vintage_column = 'Vintage', n_portfolios = 1000, minimum_return = None, maximum_return = None, minimum_risk = None, maximum_risk = None):
+# Function for running 10,000 Monte Carlo simulations and building portfolios of VC funds based on user criteria
+def simulate_portfolio_median_returns(data, n_portfolios = None, minimum_return = None, minimum_risk = None, maximum_risk = None): # n_emerging_funds, n_established_funds,
     
     portfolios_returns = []
     portfolios_risks = []
@@ -25,6 +26,7 @@ def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_establ
     optimal_portfolio_assets = None
 
     for _ in tqdm(range(n_portfolios), desc = "Simulating Portfolios...", unit = "portfolio"):
+        """
         selected_emerging = emerging_funds.sample(n = 1, replace = False)
 
         emerging_vintage_min = max(2000, selected_emerging['Vintage'].min() - 2)
@@ -39,11 +41,13 @@ def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_establ
             (established_funds['Vintage'] >= emerging_vintage_min) &
             (established_funds['Vintage'] <= emerging_vintage_max)
         ].sample(n = n_established_funds, replace = False)
-
-        portfolio_composition = pd.concat([selected_emerging, additional_emerging, selected_established])
+        """
+        funds = np.random.randint(6,20)
+        portfolio_composition = post_2000_data.sample(funds, replace=False)
+        #portfolio_composition = pd.concat([selected_emerging, additional_emerging, selected_established])
 
         returns = portfolio_composition.loc[:, 'MOIC']
-        selected_assets = portfolio_composition.loc[:, 'FUND ID'].sample(n = n_assets, replace = False).values
+        selected_assets = portfolio_composition.loc[:, 'FUND ID'].sample(n = len(portfolio_composition), replace = False).values
         
         try:
             weights = np.ones(len(selected_assets)) / len(selected_assets)
@@ -70,6 +74,26 @@ def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_establ
     optimal_portfolio_index = portfolios_risks.index(optimal_portfolio_risk)
     print(f"Portfolio Assets for Optimal Portfolio: {portfolios_assets[optimal_portfolio_index]}")
 
+    filtered_portfolios_assets = []
+    filtered_portfolios_returns = []
+    filtered_portfolios_risks = []
+    filtered_portfolios_ratios = []
+
+    for assets, return_val, risk_val, ratio_val in zip(portfolios_assets, portfolios_returns, portfolios_risks, portfolios_ratios):
+        if (return_val >= minimum_return) and (risk_val >= minimum_risk and risk_val <= maximum_risk):
+            filtered_portfolios = [
+                {
+                    'Assets': ', '.join(data[data['FUND ID'].isin(assets)]['NAME']),
+                    'Return': return_val.round(2),
+                    'Risk': risk_val.round(2),
+                    'Return-to-Risk Ratio': ratio_val
+                }
+            ]
+            filtered_portfolios_assets.append(assets)
+            filtered_portfolios_returns.append(return_val.round(2))
+            filtered_portfolios_risks.append(risk_val.round(2))
+            filtered_portfolios_ratios.append(ratio_val.round(2))
+
     fig = go.Figure()
 
     portfolio_text = [
@@ -77,16 +101,16 @@ def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_establ
         Portfolio Return: {return_val.round(2)}<br>\
         Portfolio Risk: {risk_val.round(2)}<br>\
         Portfolio Return-to-Risk Ratio: {ratio_val:.2f}"
-        for assets, return_val, risk_val, ratio_val in zip(portfolios_assets, portfolios_returns, portfolios_risks, portfolios_ratios)
+        for assets, return_val, risk_val, ratio_val in zip(filtered_portfolios_assets, filtered_portfolios_returns, filtered_portfolios_risks, filtered_portfolios_ratios)
     ]
     
     fig.add_trace(
         go.Scatter(
-            x = portfolios_risks,
-            y = portfolios_returns,
+            x = filtered_portfolios_risks,
+            y = filtered_portfolios_returns,
             mode = 'markers',
             marker = dict(
-                color = portfolios_ratios,
+                color = filtered_portfolios_ratios,
                 colorscale = 'RdBu',
                 showscale = True,
                 colorbar = dict(title = 'Risk-Return Ratio')
@@ -115,30 +139,14 @@ def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_establ
         )
 
     fig.update_layout(
-        title = f'Risk-Return Graph: {manager_type}',
+        title = f'Risk-Return Graph', #: {manager_type}
         xaxis = dict(title='Portfolio Risk (Volatility)'),
         yaxis = dict(title='Portfolio Return'),
         showlegend = True,
         legend = dict(x = 1.02, y = 1.3, traceorder = 'normal', orientation = 'v')
     )
-
-    fig.show()
     
-    overall_median_moic_across_1000_portfolios = np.nanmedian(portfolios_returns)
-
-    plt.figure(figsize = (10, 5))
-    sns.histplot(portfolios_returns, kde = True, bins = 90, color = 'blue', stat = 'density', common_norm = False)
-    plt.axvline(
-        x = overall_median_moic_across_1000_portfolios,
-        color = 'red',
-        linestyle = '--',
-        label = f'Overall Median Portfolio Return: {overall_median_moic_across_1000_portfolios:.2f} ({n_assets} funds per portfolio)'
-    )
-    plt.title(f'Benefits of Diversification between Fund Managers: ({manager_type} )')
-    plt.xlabel('Median Portfolio Return')
-    plt.ylabel('Probability Density')
-    plt.legend()
-    plt.show()
+    fig.show()
     
     filtered_portfolios = [
         {
@@ -150,20 +158,21 @@ def simulate_portfolio_median_returns(data, n_assets, n_emerging_funds, n_establ
         for assets, return_val, risk_val, ratio_val in zip(portfolios_assets, portfolios_returns, portfolios_risks, portfolios_ratios)
         if (return_val >= minimum_return) and (risk_val >= minimum_risk and risk_val <= maximum_risk)
     ]
+    
     return {'portfolio_composition': portfolio_composition, 'filtered_portfolios': filtered_portfolios}
     
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':         # Retrieving user inputs from the form
-        num_funds = int(request.form['num_funds'])
-        n_emerging = int(request.form['n_emerging'])
-        n_established = int(request.form['n_established'])
+        #num_funds = int(request.form['num_funds'])
+        #n_emerging = int(request.form['n_emerging'])
+        #n_established = int(request.form['n_established'])
+        num_simulations = int(request.form['num_simulations'])
         min_return = float(request.form['min_return'])
         min_risk = float(request.form['min_risk'])
         max_risk = float(request.form['max_risk'])
         # Calling the simulate_portfolio_median_returns function with user-entered parameters
-        plot_title = '20% Emerging & 80% Established'
-        result = simulate_portfolio_median_returns(post_2000_data, num_funds, n_emerging, n_established, plot_title, minimum_return = min_return, minimum_risk = min_risk, maximum_risk = max_risk)
+        result = simulate_portfolio_median_returns(post_2000_data,  n_portfolios = num_simulations, minimum_return = min_return, minimum_risk = min_risk, maximum_risk = max_risk) # plot_title, num_funds, n_emerging, n_established,
         filtered_portfolios = result['filtered_portfolios']
 
         optimal_portfolio_index = np.argmax([portfolio['Return-to-Risk Ratio'] for portfolio in filtered_portfolios])
@@ -176,7 +185,6 @@ def index():
 @app.route('/select_portfolios', methods=['POST'])
 def select_portfolios():
     selected_portfolios = request.form.getlist('selected_portfolios')
-    # Do something with the selected portfolios, for example, print them
     print("Selected Portfolios:", selected_portfolios)
     return redirect('/')  # Redirect to the home page or another page as needed
 
